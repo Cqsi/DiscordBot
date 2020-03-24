cad01de8ef73b55d4d939aa2ac101cacef4fdae8
Cqsi <casimirr04@gmail.com>
2020-03-24 20:56:20 +0200

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const token = "NjkyMDEzOTI2NDI2MjE0NDkz.XnpP4Q.iXEt8uteUGgEYdQe8tHDX4iZRXo";
+const token = "NjkyMDEzOTI2NDI2MjE0NDkz.XnpQyQ.B_02Y1fGT64UbWc8akmFjl3jGP0";
 
 // Configuring the bot
 client.on("ready", () =>{
